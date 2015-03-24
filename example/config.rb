0c0c4a1f3fc61deac74807f4105a60684f3d5681
Yann Esposito <yann@vigiglobe.com>
2015-03-24 10:38:55 +0100

    fix colors and config port

--- example/config.rb
+++ example/config.rb
@@ -1,7 +1,7 @@
 # Example configuration file for riemann-dash.
 
 # Serve HTTP traffic on this port
-set  :port, 4567
+set  :port, 4444
 
 # Answer queries sent to this IP address
 set  :bind, "0.0.0.0"
